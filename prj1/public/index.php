de5bd7033dd1e60bb4a9b2731b3d0f8fec7dc0c7
mt1sk <mt1sk@mail.ru>
2019-09-27 14:14:55 +0300

    app sentinel, sentinel service name, build slave config copy.

--- prj1/public/index.php
+++ prj1/public/index.php
@@ -4,14 +4,17 @@ require __DIR__.'/../vendor/autoload.php';
 
 echo('Hello world</br>');
 
-$params = ['host'=>'redis', 'port'=>'6379'];
-$options = [];
-$params = ['tcp://redis?alias=master', 'tcp://slave'];
-$options = ['replication'=>true];
+$params = ['sentinel:26379'];
+$options = ['replication'=>'sentinel', 'service'=>'my_cluster'];
 $redis = new Predis\Client($params, $options);
 $redis->connect();
 
 $redis->set('foo', date('d.m.Y H:i:s'));
 print_r($redis->get('foo').'</br>');
+
+$current = $redis->getConnection()->getCurrent()->getParameters();
+$master = $redis->getConnection()->getMaster()->getParameters();
+echo('Current: '.$master->host."<BR>");
+echo('Master: '.$master->host."<BR>");
 
 die('</BR>End.');
